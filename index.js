d3fa981e82e016a45560cbd1b8f795adf085543c
vanildoneto <107429466+vanildoneto@users.noreply.github.com>
2022-09-23 04:37:11 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+var nota1 = prompt("Digite a primeira nota=")
+var nota2 = prompt("Digite a segunda nota=")
+var nota3 = 21 - (nota1+nota2)
+
+alert('A nota minima necessaria para ser aprovado é:' + nota3)
+
